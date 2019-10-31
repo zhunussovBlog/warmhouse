78fc23cfdcdf1dff9bf61fb83e56ea91014238a6
Arsentiy <asekenskigo@gmail.com>
2019-10-31 17:55:07 +0600

    Added php files and changed others for website dynamism(multi-page)

---
+++ src/catalog.php
@@ -0,0 +1,56 @@
+<div class="catalog">
+  <a href="#" class="catalog__item boilers">Котлы</a>
+  <div class="catalog__item-hover boilers-hover">
+    <div class="catalog__item-list">
+      <img src="img/main/boiler.png" alt="" class="catalog__item-image">
+      <div class="catalog__item-info">
+        <a href="#" class="catalog__item-info__header">Газовые котлы</a>
+        <ul>
+          <li><a href="#" class="catalog__item-info__link">Protherm</a></li>
+          <li><a href="#" class="catalog__item-info__link">Vaillant</a></li>
+          <li><a href="#" class="catalog__item-info__link">Baxi</a></li>
+          <li><a href="#" class="catalog__item-info__link">Все категории</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="catalog__item-list">
+      <img src="img/main/parts-1.png" alt="" class="catalog__item-image">
+      <div class="catalog__item-info">
+        <a href="#" class="catalog__item-info__header">Комплектующие для котлов</a>
+        <ul>
+          <li><a href="#" class="catalog__item-info__link">Protherm</a></li>
+          <li><a href="#" class="catalog__item-info__link">Vaillant</a></li>
+          <li><a href="#" class="catalog__item-info__link">Baxi</a></li>
+          <li><a href="#" class="catalog__item-info__link">Все категории</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <a href="#" class="catalog__item parts">Запчасти для котлов</a>
+  <div class="catalog__item-hover parts-hover">
+    <div class="catalog__item-list">
+      <img src="img/main/boiler.png" alt="" class="catalog__item-image">
+      <div class="catalog__item-info">
+        <a href="#" class="catalog__item-info__header">Газовые котлы</a>
+        <ul>
+          <li><a href="#" class="catalog__item-info__link">Protherm</a></li>
+          <li><a href="#" class="catalog__item-info__link">Vaillant</a></li>
+          <li><a href="#" class="catalog__item-info__link">Baxi</a></li>
+          <li><a href="#" class="catalog__item-info__link">Все категории</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="catalog__item-list">
+      <img src="img/main/parts-1.png" alt="" class="catalog__item-image">
+      <div class="catalog__item-info">
+        <a href="#" class="catalog__item-info__header">Комплектующие для котлов</a>
+        <ul>
+          <li><a href="#" class="catalog__item-info__link">Protherm</a></li>
+          <li><a href="#" class="catalog__item-info__link">Vaillant</a></li>
+          <li><a href="#" class="catalog__item-info__link">Baxi</a></li>
+          <li><a href="#" class="catalog__item-info__link">Все категории</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ src/header.php
@@ -0,0 +1,94 @@
+<div class="header">
+  <a href="#" class="header-logo">
+    <img src="img/header/logo.png" alt="" class="header-logo__img">
+  </a>
+  <div class="header-info">
+    <a href="#" class="header-info__address">г. Актобе<br>Техцентр "Султан", 2/80б</a>
+    <a href="#" class="header-info__mail">info@warmhouse.kz</a>
+  </div>
+  <form action="#" class="header-form">
+    <input type="search" name="searchInput" class="header-form__search" placeholder="Поиск по товарам">
+  </form>
+  <div class="call">
+    <span class="call__icon"></span>
+    <div class="call_numbers">
+      <span class="call__number">8(747)605-99-55</span>
+      <span class="call__number">8(771)186-22-30</span>
+      <span class="call__btn">Заказать звонок</span>
+    </div>
+  </div>
+</div>
+<div class="toggles">
+  <div class="menu-service_toggle menu-toggle">
+    <a href="#" class="menu-toggle__item">
+      Техническое обслуживание
+    </a>
+    <a href="#" class="menu-toggle__item">
+      Ремонт котлов и бойлеров
+    </a>
+    <a href="#" class="menu-toggle__item">
+      Запчасти для котлов и бойлеров
+    </a>
+  </div>
+  <div class="menu-payment_toggle menu-toggle">
+    <a href="#" class="menu-toggle__item">Условия оплаты</a>
+    <a href="#" class="menu-toggle__item">Условия доставки</a>
+    <a href="#" class="menu-toggle__item">Возврат товара</a>
+  </div>
+</div>
+<div class="menu-wrapper">
+  <div class="mobile-menu_wrapper">
+    <span class="mobile-menu__catalog mobile-menu__item">Каталог</span>
+    <div class="mobile-menu__catalog_toggle mobile-menu_toggle">
+      <a href="#" class="mobile-menu_toggle__item">Котлы</a>
+      <a href="#" class="mobile-menu_toggle__item">Запчасти для котлов</a>
+    </div>
+    <span class="mobile-menu__service mobile-menu__item">Сервисный центр</span>
+    <div class="mobile-menu__service_toggle mobile-menu_toggle">
+      <a href="#" class="mobile-menu_toggle__item">Техническое обслуживание</a>
+      <a href="#" class="mobile-menu_toggle__item">Ремонт котлов и бойлеров</a>
+      <a href="#" class="mobile-menu_toggle__item">Запчасти для котлов и бойлеров</a>
+    </div>
+    <span class="mobile-menu__payment mobile-menu__item">Доставка и оплата</span>
+    <div class="mobile-menu__payment_toggle mobile-menu_toggle">
+      <a href="#" class="mobile-menu_toggle__item">Условия оплаты</a>
+      <a href="#" class="mobile-menu_toggle__item">Условия доставки</a>
+      <a href="#" class="mobile-menu_toggle__item">Возврат товара</a>
+    </div>
+    <a href="#" class="mobile-menu__contacts mobile-menu__item">Контакты</a>
+    <form action="#" class="mobile-menu_form">
+      <input type="search" class="mobile-menu_form__search" placeholder="Поиск">
+      <button class="mobile-menu_form__findBtn">Найти</button>
+    </form>
+  </div>
+</div>
+<div class="mobile-menu">
+  <div class="mobile-menu_interface">
+    <span class="mobile-menu_interface__text">Меню</span>
+    <div class="mobile-menu_interface_toggleIcon">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+</div>
+<div class="menu">
+  <a href="#" class="menu-catalog menu-item">
+    <div class="menu-catalog__icon">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    <span class="menu-catalog__text">Каталог</span>
+    <span class="menu-catalog__arrow"></span>
+  </a>
+  <a href="#" class="menu-service menu-item">
+    <span class="menu-service__text">Сервисный центр</span>
+  </a>
+  <a href="#" class="menu-payment menu-item">
+    <span class="menu-payment__text">Доставка и оплата</span>
+  </a>
+  <a href="#" class="menu-contacts menu-item">
+    <span class="menu-contacts__text">Контакты</span>
+  </a>
+</div>
